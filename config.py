15282c06e5ed0a696122d0e0f8239fd201fb92e0
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-10-04 17:52:12 +0530

    Update and rename ex_config.py to config.py

--- config.py
+++ config.py
@@ -1,32 +1,35 @@
-# EDIT THIS FILE AND RENAME TO config.py TO MAKE THIS BOT WORKING
 # FILL THESE VALUES ACCORDINGLY.
 
 from RomeoBot.config.hell_config import Config
 
 class Development(Config):
   # get these values from my.telegram.org. 
-  APP_ID = 6    # 6 is a placeholder. Fill your 6 digit api id
-  API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"   # replace this with your api hash
+  APP_ID = 9    # 9 is a placeholder. Fill your 6 digit api id
+  API_HASH = ""   # replace this with your api hash
 
   # create any PostgreSQL database.
   # I recommend to use elephantsql and paste that link here
-  DB_URI = "Your value"
-
+  DB_URI = ""
+  ALIVE_NAME = "Romeo"
   # After cloning the repo and installing requirements...
   # Do `python string.py` and fill the on screen prompts.
   # String session will be saved in your saved message of telegram.
   # Put that string here.
-  ROMEOBOT_SESSION = "Your value"
+  ROMEOBOT_SESSION = ""
 
   # Create a bot in @BotFather
   # And fill the following values with bot token and username.
-  BOT_TOKEN = "Your value" #token
-
+  BOT_TOKEN = "" #token
+  #FILL BOT USERNAME WITHOUT @
+  BOT_USERNAME = ""
   # Custom Command Handler. 
   HANDLER = "."
+ 
 
   # Custom Command Handler for sudo users.
-  SUDO_HANDLER = "!"
+  SUDO_HANDLER = "."
+  # if u want to add sudo then remove #
+  #SUDO_USERS = []
 
 
 # end of required config

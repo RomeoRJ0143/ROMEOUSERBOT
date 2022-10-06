fbac50d477252f8955a1178d883fee819425ac39
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-10-06 14:37:30 +0530

    Update __init__.py

--- RomeoBot/sql/__init__.py
+++ RomeoBot/sql/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-# from RomeoBot.config import Config
+# from UltronBot.config import Config
 
 DB_URI = os.environ.get("DATABASE_URL")
 
@@ -12,9 +12,13 @@ def start() -> scoped_session:
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
-	@@ -19,5 +21,7 @@ def start() -> scoped_session:
+    
+try:
+    BASE = declarative_base()
     SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    print("DB_URI is not configured. Features depending on the database might have issues.")
+    print(
+        "DB_URI is not configured. Features depending on the database might have issues."
+    )
     print(str(e))
